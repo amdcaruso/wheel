3442aef84b70610560f4af5306c4802b07ad3926
Amanda Cavallaro <amdcaruso@gmail.com>
2020-11-29 18:28:45 +0000

    Fixes landing on a person

--- script.js
+++ script.js
@@ -3,18 +3,32 @@
     const box = document.querySelector('.box');
     const spin = document.querySelector('.spin');
     const marker = document.querySelector('.marker');
-    const person = document.querySelector('.person');
-    const selected = document.querySelector('.selected');
 
     // Defines variables
     let deg = 0;
     let currentWinningNumber = 0;
     let teamMembers = 8;
     let segment = new Array(teamMembers);
+    let personArray = new Array(teamMembers);
     let segmentArray = [40, 80, 125, 170, 215, 260, 305, 340];
+    let segmentArrayPerson = new Array(teamMembers);
 
     // Adds Sound
     let snd = new Audio('./assets/sound.m4a');
+
+    // Selected sticker per person
+    // const person0 = document.querySelector('#box > span.person.person0');
+    // const person1 = document.querySelector('#box > span.person.person1');
+    // const person2 = document.querySelector('#box > span.person.person2');
+    // const person3 = document.querySelector('#box > span.person.person3');
+    // const person4 = document.querySelector('#box > span.person.person4');
+    // const person5 = document.querySelector('#box > span.person.person5');
+    // const person6 = document.querySelector('#box > span.person.person6');
+    // const person7 = document.querySelector('#box > span.person.person7');
+
+    for (let i = 0; i < teamMembers; i++) {
+        personArray[i] = document.querySelector(`#box > span.person.person${i}`);
+    }
 
     // Gets week day
     (function getWeekDay() {
@@ -52,6 +66,24 @@
         }
     })();
 
+    // namesEachPerson
+    (function nameEachPerson() {
+        let dictPerson = {
+            40: personArray[0],
+            80: personArray[1],
+            125: personArray[2],
+            170: personArray[3],
+            215: personArray[4],
+            260: personArray[5],
+            305: personArray[6],
+            340: personArray[7]
+        };
+
+        for (let key in dictPerson) {
+            segmentArrayPerson[key] = dictPerson[key];
+        }
+    })();
+
     // Lands on a person
     function hasFallenIntoThatRange() {
         currentWinningNumber = Math.floor(Math.random() * segmentArray.length);
@@ -75,19 +107,18 @@
         }
     }
 
-    // Function to make the selected sticker visible
-    function showSelected() {
-        if (selected.style.display === 'none') {
-            selected.style.display = 'block';
-        }
+    // Makes the selected sticker visible
+    function addsSticker(person) {
+        let url = './assets/images/sticker.png';
+        let image = new Image();
+        image.src = url;
+        image.className = 'selected';
+
+        let toBeAppended = segmentArrayPerson[person];
+        toBeAppended.appendChild(image);
     }
 
-    // Adds the selected sticker on the person's segment
-    person.addEventListener('click', () => {
-        showSelected();
-    });
-
-    // On click on spin
+    // On click on the spin button
     spin.addEventListener('click', () => {
         hasEveryOneBeenSelected();
         spin.getElementsByClassName.pointerEvents = 'none';
@@ -95,8 +126,8 @@
         deg = 180 - hasFallenIntoThatRange();
         box.classList.add('animateBox');
         box.style.transform = `rotate(${deg}deg)`;
-
         if (deg > 0) snd.play();
+        addsSticker(landedNumber);
     });
 
     // When the transition ends
